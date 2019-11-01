0f826bc8d164689a411da72647a10e151044ec82
jonstenn <jonsteinn@gmail.com>
2019-11-01 11:21:23 -0400

    git_script_test

--- lazy_git.sh
+++ lazy_git.sh
@@ -10,7 +10,4 @@ make -C ./src clean
 
 git add .
 git commit -m "$1"
-git config user.email "jonsteinn@gmail.com"
-git config user.name "jonstenn"
-git config user.password "$2"
-git push origin master
+git push "https://jonsteinn:$2@github.com/jonsteinn/c-game.git"
