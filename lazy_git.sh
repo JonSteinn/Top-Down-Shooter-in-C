8e62386b230df7a035872ec5b17ff12512367b48
JonSteinn <jonsteinn@gmail.com>
2019-11-01 11:17:27 -0400

    git_script_test

--- lazy_git.sh
+++ lazy_git.sh
@@ -10,4 +10,7 @@ make -C ./src clean
 
 git add .
 git commit -m "$1"
-git push origin master <<< $'jonsteinn\n$2'
+git config user.email "jonsteinn@gmail.com"
+git config user.name "jonstenn"
+git config user.password "$2"
+git push origin master
