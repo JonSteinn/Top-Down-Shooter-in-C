51fecfc57ff061aa71eddf939a6182a6a0e46655
jonstenn <jonsteinn@gmail.com>
2019-11-04 18:13:57 -0500

    rotation2

--- src/gmath.c
+++ src/gmath.c
@@ -9,14 +9,14 @@ float carmack_inverse_sqrt(float x)
 
 	x2 = x * 0.5F;
 	y  = x;
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wstrict-aliasing"
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wstrict-aliasing"
 	i  = * (( long * ) &y);                       // evil floating point bit level hacking
 	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
 	y  = * ( float * ) &i;
-    #pragma GCC diagnostic pop
+	#pragma GCC diagnostic pop
 	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+	//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
 	return y;
 }
