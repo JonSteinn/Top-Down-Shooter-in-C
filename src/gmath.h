41d1ea2149a74cd40849cb9855e487cb3d07bbba
jonstenn <jonsteinn@gmail.com>
2019-11-04 21:03:39 -0500

    cleanup2

--- src/gmath.h
+++ src/gmath.h
@@ -1,6 +1,14 @@
 #ifndef xJdkK3dms1_GMATH_H
 #define xJdkK3dms1_GMATH_H
 
+/**
+ * A two value tuple.
+ * 
+ *  x
+ *      The horizontal coordinate
+ *  y
+ *      The vertical coordinate
+ */
 typedef struct {
     float x;
     float y;
@@ -10,10 +18,24 @@ typedef Vector2d Point2d;
 
 static const float gPI = 3.14159265358979f;
 
-
+/**
+ * John Carmack's infamous inverse sqrt from Quake3 Arena.
+ */
 float carmack_inverse_sqrt(float x);
+
+/**
+ * A fast approximation of arccos.
+ */
 float fast_acos(float x);
+
+/**
+ * Converts radians to degrees.
+ */
 float rad_to_deg(float rad);
+
+/**
+ * Returns 1 if x is positive, -1 if x is negative and 0 otherwise.
+ */
 int sign(float x);
 
 #endif
--- src/player.h
+++ src/player.h
@@ -7,16 +7,40 @@
 #include "gevent.h"
 #include "gmath.h"
 
+/**
+ * The player object.
+ * 
+ *  position
+ *      The 2d coordinate of the player
+ *  rotation
+ *      The angle from 'east' to the direction of the player
+ *  texture
+ *      The player sprite
+ */
 typedef struct {
     Point2d*        position;
     float           rotation;
-
     SDL_Texture*    texture;
 } Player;
 
+/**
+ * Initialize the player object by allocating memory and loading resources.
+ */
 Player* init_player(SDL_Renderer* renderer, float x, float y);
+
+/**
+ * Update the player based on events.
+ */
 void update_player(Player* player, GameEvents* gevts, float dt);
+
+/**
+ * Draw the player.
+ */
 void draw_player(SDL_Renderer* renderer, Player* player);
+
+/**
+ * Free any resources used by the player.
+ */
 void destroy_player(Player* player);
 
 #endif
