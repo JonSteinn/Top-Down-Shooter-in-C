dc89866bf103a98d9632592e620ed89ee72889bc
JonSteinn <jonsteinn@gmail.com>
2019-11-07 14:54:44 -0500

    cleanup1

--- src/main.c
+++ src/main.c
@@ -2,25 +2,25 @@
 #include "game.h"
 
 /**
- * The program's starting point.
+ * Function:
+ *  main
+ * 
+ * Purpose:
+ *  The program's starting point.
+ * 
+ * argc: 
+ *  The number of arguments 
+ * args:
+ *  The list of arguments
+ * 
+ * returns: 
+ *  0 on succcess, 1 otherwise.
  */
 int32_t main(int32_t argc, char** args) {
+
+    foo();
     Game* game = init_game(argc, args);
     start_game(game);
     destroy_game(game);
     return 0;
 }
-
-/*
-some rand strings
-
-okgLyTpWkI
-YhFtlVUKnb
-wzoKbdm408
-oFlVH9GDPt
-x9bfTkfyVx
-wHBqrj2E14
-eOXY0hjAhB
-ks2Ye6goo7
-
-*/
